22839c2bcc71779dd8c88ebb6d73088abc4b66c8
alex-candela <alex.candela@outlook.com>
2024-02-19 14:44:12 +0100

    update: styles and article summarize logic

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,19 +1,32 @@
+import React from "react";
+
 import { logo } from "../assets";
 
 const Hero = () => {
     return (
         <header className="w-full flex justify-center items-center flex-col">
-            <nav className="w-full flex justify-between items-center mb-10 pt-3">
-                <img src={logo} alt="sumz_logo" className="w-28 object-contain" />
-                <button type="button" className="black_btn" onClick={() => window.open("https://github.com/Alejandro-Candela")}>
-                    Github
+            <nav className="flex justify-between items-center w-full mb-10 pt-3">
+                <a href="/">
+                    <img src={logo} alt="sumz_logo" className="w-20 object-contain" />
+                </a>
+
+                <button
+                    type="button"
+                    onClick={() => window.open("https://github.com/Alejandro-Candela", "_blank")}
+                    className="black_btn"
+                >
+                    GitHub
                 </button>
             </nav>
+
             <h1 className="head_text">
                 Summarize Articles with <br className="max-md:hidden" />
-                <span className="orange_gradient">OpenAI GPT-4</span>
+                <span className="orange_gradient ">OpenAI GPT-4</span>
             </h1>
-            <h2 className="desc">Simplify your reading with Summarize, an open-source article summarizer</h2>
+            <h2 className="desc">
+                Simplify your reading with Summize, an open-source article summarizer that
+                transforms lengthy articles into clear and concise summaries
+            </h2>
         </header>
     );
 };
