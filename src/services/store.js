d9853f821b034af3e91e82e93360d2064698f7f7
alex-candela <alex.candela@outlook.com>
2024-02-19 13:15:51 +0100

    API query created

--- src/services/store.js
+++ src/services/store.js
@@ -3,5 +3,5 @@ import { articleApi } from "./article";
 
 export const store = configureStore({
     reducer: {[articleApi.reducerPath]: articleApi.reducer},
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(),
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(articleApi.middleware),
 });
